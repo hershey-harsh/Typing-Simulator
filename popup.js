24a86f7b979c7be801cf5ea84daa189d695ed7f7
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2024-07-23 13:37:58 -0400

    Temporary popup HTML
    Made to access the bigger dashboard which will open in another tab.

---
+++ popup.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css">
+	<script src="popup.js"></script>
+</head>
+
+<body>
+
+	<button type="submit" id="notify-button" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Dashboard</button>
+
+</body>
+</html>
